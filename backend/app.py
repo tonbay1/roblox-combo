c54ea9065b7d84de86af029a7c4c829754129d66
tonbay1 <54112293+tonbay1@users.noreply.github.com>
2025-05-08 16:03:09 +0700

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -27,4 +27,6 @@ def validate_combos():
     return jsonify(results)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
